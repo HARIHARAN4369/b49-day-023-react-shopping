83ba9b43375df3304040ffad4f2d6118c7e55353
HARIHARAN4369 <hariharan4369@gmail.com>
2022-10-23 00:00:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+ import './App.css';
+//  import Nav from './components/Nav';
+ import Home from './components/Home';
+
+
+ function App() {
+  return <div>
+ {/* <Nav/> */}
+ <Home/>
+  </div>
+}
+
+export default App;
+ 
